d1902a3a2d425487237b79cf8c486f9aa21bdcaa
samuelhackwill <hackmew@gmail.com>
2023-04-07 00:32:28 +0200

    Add concept for stickers

--- imports/ui/layouts/feed.js
+++ imports/ui/layouts/feed.js
@@ -2,11 +2,12 @@ import './feed.html'
 
 import '../components/card.js'
 import '../components/bubble.js'
+import '../components/sticker.js'
 
 data = new ReactiveVar()
 
 Template.feed.helpers({
-	getBubbles(){
+	feedItems(){
         return data.get()
 	}
 })
@@ -15,7 +16,7 @@ Template.feed.helpers({
 Template.feed.onRendered(function(){
     document.getElementById("feed")._uihooks = {
         insertElement: (node, next) => {
-            // console.log("DOM INSERTION DETECTED", "NODE ", node, "NEXT ", next.parentNode)
+            console.log("DOM INSERTION DETECTED", "NODE ", node, "NEXT ", next.parentNode)
             next.parentNode.appendChild(node)
 
             setTimeout(function(){
@@ -25,10 +26,13 @@ Template.feed.onRendered(function(){
     }
 })
 
-addData = function(){
+addData = function(type){
     // this function is used to add data to the local reactive Var, which is used to populate the feed.
     // we will need to get the data from the proper database next ("scenarios") rather than just inserting random stuff like we're doing now.
     tempArr = data.get() || []
-    tempArr.push({left:false, who:"Samuel", text:"BLABLABLABA."})
+    obj = {left:true, who:"Samuel", text:"BLABLABLABA."}
+    obj[type] = true
+    
+    tempArr.push(obj)
     data.set(tempArr)
 }
