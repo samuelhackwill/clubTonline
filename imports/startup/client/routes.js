ad81865b141c98ac9828ec0d6ec4ed9a0541fbeb
samuelhackwill <hackmew@gmail.com>
2023-04-14 14:33:33 +0200

    Change stuff after meteor meltdown

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,5 +1,4 @@
 import { FlowRouter } from "meteor/ostrio:flow-router-extra";
-import { BlazeLayout } from "meteor/kadira:blaze-layout";
 
 // import '../../ui/pages/song.js';
 // import '../../ui/pages/office.js';
@@ -9,6 +8,6 @@ import "../../ui/pages/show.js";
 FlowRouter.route("/show", {
   name: "show",
   action() {
-    BlazeLayout.render("show");
-  },
+      this.render('show');
+    },
 });
