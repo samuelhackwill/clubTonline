971a865a5f8ab2f7e9e97b65a766305203fc8031
samuelhackwill <hackmew@gmail.com>
2023-04-21 13:00:42 +0200

    Add home view

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,12 +2,19 @@ import { FlowRouter } from "meteor/ostrio:flow-router-extra";
 
 // import '../../ui/pages/song.js';
 // import '../../ui/pages/office.js';
-// import '../../ui/pages/home.js';
+import '../../ui/pages/home.js';
 import "../../ui/pages/show.js";
 
 FlowRouter.route("/show", {
   name: "show",
   action() {
       this.render('show');
+    },
+});
+
+FlowRouter.route("/", {
+  name: "home",
+  action() {
+      this.render('home');
     },
 });
