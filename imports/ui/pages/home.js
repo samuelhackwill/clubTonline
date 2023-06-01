bf44f1391e10c47258dbd99b8f5e2791b4dae8e5
samuelhackwill <hackmew@gmail.com>
2023-06-01 15:44:30 +0200

    Add matomo and small changes in text from mathilde

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -2,20 +2,16 @@ import "./home.html"
 
 const waitingData = [
     {autoText:true, name:"bot-mathilde-1" ,text:
-        ["Bonjour, bienvenue au club travail online.",
+        ["Bonjour, bienvenue sur le guichet online du club travail.",
         "Je suis Samuel, le conseiller club travail spécialisé dans les réseaux informatiques.",
-        "Mes camarades Mathilde Maillard et Thomas Bris arriveront bientôt pour animer cette expérience en ligne",
-        "où l'on se posera notamment la question de savoir si le travail est à l'intérieur ou à l'extérieur de nous,",
+        "Avec mes collègues Thomas et Mathilde, nous sommes en train de préparer un nouvel espace bien-être, glamour & travail",
+        "où l'on se posera notamment la question de savoir si le travail est à l'intérieur ou à l'extérieur de nous",
         "quel bruit fait notre travail, ou encore",
         "s'il est bien vrai que 'nul n'est si pauvre qu'il ne doive s'asseoir sur une citrouille.'",
         "comme le disait Thoreau.",
-        "Nous vous donnons rendez-vous ici, la semaine du 19 Juin,",
-        "pendant la pause café,",
-        "à votre bureau ",
-        "chez vous,",
-        "ou sur vos genoux où que vous soyez,",
-        "sur guichet.clubtravail.be",
-        "si vous voulez recevoir un mail quand le site sera prêt, vous pouvez vous inscrire ci-dessous :)",
+        "Nous vous donnons rendez-vous ici bientôt",
+        "dès la fin juin",
+        "Si vous voulez être tenu au courant de l'ouverture du guichet en ligne, vous pouvez vous inscrire ci-dessous :",
     ]},
     {autoText:true, name:"mailForm", form:true},
     {autoText:true, name:"linkNoFuturs", link:true}
