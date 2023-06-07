48047881b4d82b86410433c491b4af02e7d3ad15
samuelhackwill <hackmew@gmail.com>
2023-06-07 17:51:31 +0200

    Remove deck of cards logic in the feed

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -1,7 +1,7 @@
 import "./home.html"
 
 const waitingData = [
-    {autoText:true, name:"bot-mathilde-1" ,text:
+    {name:"bot-mathilde-1" ,text:
         ["Bonjour, bienvenue sur le guichet online du club travail.",
         "Je suis Samuel, le conseiller club travail spécialisé dans les réseaux informatiques.",
         "Avec mes collègues Thomas et Mathilde, nous sommes en train de préparer un nouvel espace bien-être, glamour & travail",
@@ -13,8 +13,8 @@ const waitingData = [
         "dès la fin juin",
         "Si vous voulez être tenu au courant de l'ouverture du guichet en ligne, vous pouvez vous inscrire ci-dessous :",
     ]},
-    {autoText:true, name:"mailForm", form:true},
-    {autoText:true, name:"linkNoFuturs", link:true}
+    {name:"mailForm", form:true},
+    {name:"linkNoFuturs", link:true}
 ]
 
 export const dataFridge = new ReactiveVar(waitingData);
