be8c67472d500c5470f4922e90e5692b8d7a3032
samuelhackwill <hackmew@gmail.com>
2023-06-10 11:07:59 +0200

    Add npm scripts that were missing (for linting)

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,6 +1,6 @@
 import { FlowRouter } from "meteor/ostrio:flow-router-extra";
 
-import '../../ui/pages/home.js';
+import "../../ui/pages/home.js";
 import "../../ui/pages/show.js";
 import "../../ui/pages/songs/berceuse.js";
 import "../../ui/pages/songs/karaoke.js";
@@ -9,35 +9,34 @@ import "../../ui/pages/songs/lettre.js";
 FlowRouter.route("/show", {
   name: "show",
   action() {
-      this.render('show');
-    },
+    this.render("show");
+  },
 });
 
 FlowRouter.route("/", {
   name: "home",
   action() {
-      this.render('home');
-    },
+    this.render("home");
+  },
 });
 
 FlowRouter.route("/berceuse", {
   name: "berceuse",
   action() {
-      this.render('berceuse');
-    },
+    this.render("berceuse");
+  },
 });
 
 FlowRouter.route("/karaoke", {
   name: "karaoke",
   action() {
-      this.render('karaoke');
-    },
+    this.render("karaoke");
+  },
 });
 
 FlowRouter.route("/lettre", {
   name: "lettre",
   action() {
-      this.render('lettre');
-    },
+    this.render("lettre");
+  },
 });
-
--- imports/ui/components/staticBubble.js
+++ imports/ui/components/staticBubble.js
@@ -1,40 +1,43 @@
 import "./staticBubble.html";
-import {Mailing} from "../../API/mailing/mailing.js"
+import { Mailing } from "../../API/mailing/mailing.js";
 
-Template.staticBubble.onCreated(function () {
-  });
-  
+Template.staticBubble.onCreated(function () {});
+
 Template.staticBubble.events({
-  'submit .subscribe'(event) {
+  "submit .subscribe"(event) {
     // Prevent default browser form submit
     event.preventDefault();
 
-    input = event.currentTarget[0]
+    input = event.currentTarget[0];
 
-    Meteor.call("insertMail", input.value)
+    Meteor.call("insertMail", input.value);
 
     // Clear form
-    document.getElementById("inline-mail").value = '';
-    document.getElementById("subscriptionButton").innerHTML = 'Inscription OK!';
-    document.getElementById("subscriptionButton").classList.add("bg-green-500", "hover:bg-green-400")
-    document.getElementById("subscriptionButton").classList.remove("bg-purple-500", "hover:bg-purple-400")
-  }
-})
+    document.getElementById("inline-mail").value = "";
+    document.getElementById("subscriptionButton").innerHTML = "Inscription OK!";
+    document
+      .getElementById("subscriptionButton")
+      .classList.add("bg-green-500", "hover:bg-green-400");
+    document
+      .getElementById("subscriptionButton")
+      .classList.remove("bg-purple-500", "hover:bg-purple-400");
+  },
+});
 
 Template.staticBubble.helpers({
-  isSubscribe(){
+  isSubscribe() {
     if (this.name != undefined && this.name == "mailForm") {
-      return true
+      return true;
     }
   },
-  isLink(){
+  isLink() {
     if (this.name != undefined && this.name == "linkNoFuturs") {
-      return true
+      return true;
     }
   },
-  isTextOnly(){
+  isTextOnly() {
     if (this.name == undefined) {
-      return true
+      return true;
     }
   },
-})
+});
