b1c3cb03ab693433c194135b637ee3495b3b7f20
Samuel <hackmew@gmail.com>
2023-06-14 11:49:38 +0200

    Add mouse events to turn answers, wip

--- imports/ui/components/dataBubble.js
+++ imports/ui/components/dataBubble.js
@@ -2,7 +2,7 @@ import "./dataBubble.html";
 
 Template.dataBubble.onCreated(function(){  
     this.autorun(() => {
-        // maybe ONLY subscribe to answers in the proper scenario to avoid also subscribing to all th edocuments qui concernent d'autres questions?
+        // maybe ONLY subscribe to answers in the proper scenario to avoid also subscribing to all the documents qui concernent d'autres questions?
 
         //   this.subscribe('todos.inList');
     });
