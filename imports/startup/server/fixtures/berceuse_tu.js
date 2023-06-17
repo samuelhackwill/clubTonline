3215e71dbd95718d299de0d242d66f1c4bda9e40
Samuel <hackmew@gmail.com>
2023-06-17 16:41:18 +0200

    Add v1 of berceuse fixture

--- imports/startup/server/fixtures/berceuse_tu.js
+++ imports/startup/server/fixtures/berceuse_tu.js
@@ -7,7 +7,7 @@ export const berceuse_tu = [
     {
       type: "SB",
       text:
-        "Je te parle depuis la forêt de Soignes en lisière de Bruxelles. Il fait bon et ça sent presque le persil. ",
+        "Je te parle depuis la forêt de Soignes en lisière de Bruxelles. Il fait bon et ça sent presque le persil.",
     },
     {          
       type: "---BB---",
@@ -37,4 +37,145 @@ export const berceuse_tu = [
       label:
       "Si tu pouvais changer de posture, ce serait laquelle ?",
   },
+  {
+    type: "SB",
+    text:
+      "« La posture professionnelle se façonne à partir de l'intention de satisfaire aux normes professionnelles. C'est un processus d'ajustement permanent, une recherche de cohérence, d'équilibre entre des éléments personnels et les attentes de la sphère professionnelle. »",
+  },
+  {          
+    type: "---BB---",
+    name: "card.berceuse_proPosture",
+    size: "s",
+    label:
+    "Est-ce que tu penses que ta posture professionnelle influence ta posture corporelle ?",
+  },
+  {          
+    type: "---BB---",
+    name: "card.berceuse_posture2",
+    size: "s",
+    label:
+    "Je me permets alors de te reposer la question :  si tu pouvais changer de posture, ce serait laquelle ? ",
+  },
+  {          
+    type: "---BB---",
+    name: "card.berceuse_legere",
+    size: "s",
+    label:
+    "Peux-tu maintenant nous décrire en une phrase une fois où tu t’es senti.e Léger (Légère) dans tes mouvements? Décris-moi cette sensation.",
+  },
+  {          
+    type: "---BB---",
+    name: "card.berceuse_legere",
+    size: "s",
+    label:
+    "Tiens, je me demandais, ça t’es déjà arrivé de partir, sans savoir si tu allais revenir ? De monter dans un bus sans connaitre sa destination ?",
+  },
+  {          
+    type: "---BB---",
+    name: "card.berceuse_quitter",
+    size: "s",
+    label:
+    "Qu’est-ce qui t’empêche de te lever là maintenant et de tout quitter ?",
+  },
+  {          
+    type: "---BB---",
+    name: "card.berceuse_retient",
+    size: "s",
+    label:
+    "Quelle est la principale chose qui te retient ?",
+  },
+  {
+    type: "---BB---",
+    save:true,
+    name: "card.berceuse_pseudo",
+    size: "s",
+    label:
+    "Tiens, au fait, comment souhaites-tu qu’on t’appelle ici ?",
+  },
+  {
+    type: "---BB---",
+    name: "card.berceuse_identite",
+    size: "s",
+    label:
+    "Est-ce que tu penses que ton prénom contient toute ton identité ?",
+  },
+  {
+    type: "---BB---",
+    name: "card.berceuse_aptonyme2",
+    size: "s",
+    label:
+    "Est-ce que ton prénom ou ton nom ont un lien quelconque avec ton travail ? ",
+  },
+  {
+    type: "---BB---",
+    name: "card.berceuse_sommeil",
+    size: "s",
+    label:
+    "Comment est ton sommeil ?",
+  },
+  {
+    type: "---BB---",
+    name: "card.berceuse_insomnia",
+    size: "s",
+    label:
+    "Est-ce que ton travail t’empêche de dormir ?",
+  },
+  {
+    type: "---BB---",
+    name: "card.berceuse_travailleDodo",
+    size: "s",
+    label:
+    "Est-ce que tu travailles en dormant ?",
+  },
+  {
+    type: "---BB---",
+    name: "card.berceuse_dormirTravailler",
+    size: "s",
+    label:
+    "Est-ce que dormir c’est travailler ?",
+  },
+  {
+    type: "---BB---",
+    name: "card.berceuse_reveRecent",
+    size: "s",
+    label:
+    "Raconte nous un rêve récent.",
+  },
+  {
+    type: "---BB---",
+    name: "card.berceuse_mieuxDormir",
+    size: "s",
+    label:
+    "Prends un petit temps pour réfléchir et écrire ce que tu te souhaites pour mieux dormir.",
+  },
+  {
+    type: "---BB---",
+    save:true,
+    name: "card.berceuse_deuxMots",
+    size: "s",
+    label:
+    "Est-ce que tu pourrais donner deux mots qui résument ce souhait ?",
+  },
+  {
+    type: "SB",
+    text:
+    "<insect sounds>",
+  },
+  {
+    type: "SB",
+    text:
+    "Maintenant, tu vas pouvoir t’allonger là où tu trouves, auprès des insectes",
+  },
+  {
+    type: "SB",
+    text:
+    "Mathilde : tu vas pouvoir souffler trois fois très profondément en t’allonger sur le sol en prenant ton ordinateur au- dessus de toi, si c’est possible. Et cliquez sur le bouton.",
+  },  
+  {
+    type: "---BB---",
+    name: "end",
+    label:
+    "conclure",
+  },
+
 ]
