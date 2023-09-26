2502a19068bdb63cf6549d2839762eedb0fe9706
samuelhackwill <hackmew@gmail.com>
2023-09-26 20:03:33 +0200

    Change the color of footer on completed page

--- imports/ui/pages/completed.js
+++ imports/ui/pages/completed.js
@@ -1,37 +1,38 @@
-import "./completed.html"
+import "./completed.html";
 
 const outro = [
-    {
-        type: "SB",
-        text: "Wah super merci d'avoir participé à club travail online!.",
-    },
-    {
-        type: "SB",
-        text: "c'était super, ben dites-donc on s'est bien amusey",
-    },
-    {
-        type: "SB",
-        text: "tenez regardez ça c'est toutes les autres chansons qui ont été écrites par les autres pélos:",
-    },
-    {
-        type: "---BB---",
-        name: "getAllSongs",
-        label: "Chargement de toutes les chansons...",
-    },    
-]  
+  {
+    type: "SB",
+    text: "Wah super merci d'avoir participé à club travail online!.",
+  },
+  {
+    type: "SB",
+    text: "c'était super, ben dites-donc on s'est bien amusey",
+  },
+  {
+    type: "SB",
+    text: "tenez regardez ça c'est toutes les autres chansons qui ont été écrites par les autres pélos:",
+  },
+  {
+    type: "---BB---",
+    name: "getAllSongs",
+    label: "Chargement de toutes les chansons...",
+  },
+];
 
 Template.completed.helpers({
-    endData() {
-      return outro;
-    },
-  });
+  endData() {
+    return outro;
+  },
+});
 
-
-Template.completed.onRendered(function(){
+Template.completed.onRendered(function () {
   // Call the function to replace Y scrolling with X scrolling
   replaceYScrollWithXScroll();
-  setTimeout(function(){
-    document.body.classList.remove("bg-yellow-clubT")
-    document.body.classList.add("bg-indigo-200")
-  },0)
-})
+  setTimeout(function () {
+    document.body.classList.remove("bg-yellow-clubT");
+    document.body.classList.add("bg-indigo-200");
+    document.getElementById("footer").classList.remove("bg-yellow-clubT");
+    document.getElementById("footer").classList.add("bg-indigo-200");
+  }, 0);
+});
